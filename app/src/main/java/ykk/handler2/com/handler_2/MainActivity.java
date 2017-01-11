d74703d86349fe7e85ddc0aed539b2d664279d6e
kailovewei <1045110506@qq.com>
2017-01-11 23:17:42 +0800

    ykk description!

--- app/src/main/java/ykk/handler2/com/handler_2/MainActivity.java
+++ app/src/main/java/ykk/handler2/com/handler_2/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView textView;
     private Button button;
-    private  static  Handler handler;
+    private  Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
